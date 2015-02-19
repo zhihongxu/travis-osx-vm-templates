7b9306dc2b53857a1b859653fb9ee0d4413ed1cf
Kamil Trzcinski <ayufan@ayufan.eu>
2015-02-19 13:20:53 +0100

    Properly handle Xcode Beta

--- scripts/xcode.sh
+++ scripts/xcode.sh
@@ -3,6 +3,11 @@
 set -eo pipefail
 shopt -s nullglob
 
+if [[ -d "/Applications/Xcode Beta.app" ]]; then
+    echo "Moving Xcode Beta..."
+    mv "/Applications/Xcode Beta.app" "/Applications/Xcode.app"
+fi
+
 [[ ! -d "/Applications/Xcode.app" ]] && exit
 
 expand_dmg() {
